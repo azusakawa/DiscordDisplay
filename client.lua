40a08fb0501a1eae98ffb33ee9134326bf2fb56b
azusakawa <58968500+azusakawa@users.noreply.github.com>
2019-12-17 19:09:59 +0800

    Add files via upload
    Download:

---
+++ client.lua
@@ -0,0 +1,56 @@
+Citizen.CreateThread(function()
+	while true do
+		SetDiscordAppId(644048182904487946)
+		SetDiscordRichPresenceAsset('logo')
+        SetDiscordRichPresenceAssetText('')
+        SetDiscordRichPresenceAssetSmall('small')
+        SetDiscordRichPresenceAssetSmallText('')
+		Citizen.Wait(1000)
+	end
+end)
+Citizen.CreateThread(function()
+	while true do
+		local VehName = GetLabelText(GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsUsing(PlayerPedId()))))
+		if VehName == "NULL" then VehName = GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsUsing(PlayerPedId()))) end
+		local x,y,z = table.unpack(GetEntityCoords(PlayerPedId(),true))
+		local StreetHash = GetStreetNameAtCoord(x, y, z)
+		local pId = GetPlayerServerId(PlayerId())
+		local pName = GetPlayerName(PlayerId())
+		Citizen.Wait(1000)
+		if StreetHash ~= nil then
+			StreetName = GetStreetNameFromHashKey(StreetHash)
+			if IsPedOnFoot(PlayerPedId()) and not IsEntityInWater(PlayerPedId()) then
+				if IsPedSprinting(PlayerPedId()) then
+					SetRichPresence("ID: "..pId.." | "..pName.." is sprinting down "..StreetName)
+				elseif IsPedRunning(PlayerPedId()) then
+					SetRichPresence("ID: "..pId.." | "..pName.." is running down "..StreetName)
+				elseif IsPedWalking(PlayerPedId()) then
+					SetRichPresence("ID: "..pId.." | "..pName.." is walking down "..StreetName)
+				elseif IsPedStill(PlayerPedId()) then
+					SetRichPresence("ID: "..pId.." | "..pName.." is standing on "..StreetName.."")
+				end
+			elseif GetVehiclePedIsUsing(PlayerPedId()) ~= nil and not IsPedInAnyHeli(PlayerPedId()) and not IsPedInAnyPlane(PlayerPedId()) and not IsPedOnFoot(PlayerPedId()) and not IsPedInAnySub(PlayerPedId()) and not IsPedInAnyBoat(PlayerPedId()) then
+				local MPH = math.ceil(GetEntitySpeed(GetVehiclePedIsUsing(PlayerPedId())) * 2.236936)
+				if MPH > 50 then
+					SetRichPresence("ID: "..pId.." | "..pName.." is speeding down "..StreetName.." at "..MPH.."MPH in a "..VehName)
+				elseif MPH <= 50 and MPH > 0 then
+					SetRichPresence("ID: "..pId.." | "..pName.." is cruising down "..StreetName.." at "..MPH.."MPH in a "..VehName)
+				elseif MPH == 0 then
+					SetRichPresence("ID: "..pId.." | "..pName.." is parked on "..StreetName.." in a "..VehName)
+				end
+			elseif IsPedInAnyHeli(PlayerPedId()) or IsPedInAnyPlane(PlayerPedId()) then
+				if IsEntityInAir(GetVehiclePedIsUsing(PlayerPedId())) or GetEntityHeightAboveGround(GetVehiclePedIsUsing(PlayerPedId())) > 5.0 then
+					SetRichPresence("ID: "..pId.." | "..pName.." is flying over "..StreetName.." in a "..VehName)
+				else
+					SetRichPresence("ID: "..pId.." | "..pName.." is landed at "..StreetName.." in a "..VehName)
+				end
+			elseif IsEntityInWater(PlayerPedId()) then
+				SetRichPresence("ID: "..pId.." | "..pName.." is swimming")
+			elseif IsPedInAnyBoat(PlayerPedId()) and IsEntityInWater(GetVehiclePedIsUsing(PlayerPedId())) then
+				SetRichPresence("ID: "..pId.." | "..pName.." is sailing in a "..VehName)
+			elseif IsPedInAnySub(PlayerPedId()) and IsEntityInWater(GetVehiclePedIsUsing(PlayerPedId())) then
+				SetRichPresence("ID: "..pId.." | "..pName.." is in a yellow submarine")
+			end
+		end
+	end
+end)
